4ee72301afef1570d94560c7f4bd55d069440225
Dick Tonye <djoeton@gmail.com>
2024-01-14 15:49:47 +0100

    fixed navbar responsiveness

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,23 +1,49 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './nav.css';
 
 export default function Header() {
   const [activeLink, setActiveLink] = useState('');
+  const [isMobile, setIsMobile] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     setActiveLink(location.pathname);
   }, [location.pathname]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const navMenusWrapperStyle = {
+    transitionProperty: isNavOpen ? 'left' : 'none',
+  };
+  
+
   return (
       <div className="header header-light dark-text">
           <div className="container">
-              <nav id="navigation" className="navigation navigation-landscape">
+              <nav id="navigation" className={`navigation ${isMobile ? 'navigation-portrait' : 'navigation-landscape'}`}>
                   <div className="nav-header">
                       <Link className="nav-brand" to="#">
                           <img src="assets/img/logo-dark.png" className="logo" alt="" />
                       </Link>
-                      <div className="nav-toggle"></div>
+                      <div className="nav-toggle" onClick={toggleNav}></div>
                       <div className="mobile_nav">
                           <ul>
                               <li>
@@ -28,7 +54,7 @@ export default function Header() {
                           </ul>
                       </div>
                   </div>
-                  <div className="nav-menus-wrapper">
+                  <div className={`nav-menus-wrapper ${isNavOpen ? 'nav-menus-wrapper-open' : ''}`} style={navMenusWrapperStyle}>
                       <ul className="nav-menu">
                           <li className={activeLink === '/' ? 'active' : ''}><Link to="/">Home</Link></li>
                           <li className={activeLink === '/about' ? 'active' : ''}><Link to="/about">About</Link></li>
@@ -51,6 +77,7 @@ export default function Header() {
 
                       </ul>
                   </div>
+                  <div className={`nav-overlay-panel ${isNavOpen ? 'nav-overlay-panel-visible' : ''}`} />
               </nav>
           </div>
       </div>
