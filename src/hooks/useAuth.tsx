356b72eb2319bb41872e56daa6e776e70dd01aa5
Tonye DICK <31224101+tonyedick@users.noreply.github.com>
2025-07-08 16:40:37 +0100

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect, createContext, useContext } from 'react';
 import { User, Session } from '@supabase/supabase-js';
 import { supabase } from '../integrations/supabase/client';
